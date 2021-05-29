d8274dc98422ed125d7502792e596befa8180413
jaiverma <jai2.verma@outlook.com>
2021-05-29 19:35:01 +0530

    Updating queries to work with newer API

--- flow/three/solution/q.scala
+++ flow/three/solution/q.scala
@@ -1,12 +1,12 @@
 def getFlow() = {
     val methods = cpg.method.name("memcpy").caller
-    val filteredMethods = methods.l.filter(
+    val filteredMethods = methods.filter(
         method => {
-            val src = method.start.parameter
-            val sink = method.start.ast.isCallTo("memcpy").argument.order(3)
+            val src = method.parameter
+            val sink = method.ast.isCallTo("memcpy").argument(3)
             sink.reachableBy(src)
         }.size > 0
-    ).start
+    )
 
     val src = cpg.call.name("ntohl")
     val sink = filteredMethods.parameter.argument
--- flow/two/solution/q.scala
+++ flow/two/solution/q.scala
@@ -4,16 +4,16 @@ def getFlow() = {
 
     // we want to filter those methods where the value returned by
     // ntohl is returned by the method
-    val filteredMethods = methods.l.filter(
+    val filteredMethods = methods.filter(
         method => {
-            val src = method.start.ast.isCallTo("ntohl")
-            val sink = method.start.methodReturn
+            val src = method.ast.isCallTo("ntohl")
+            val sink = method.methodReturn
             sink.reachableBy(src)
         }.size > 0
-    ).start
-    
+    )
+
     // we will treat call to these filtered methods as good as a call to
-    // ntohl. this is will only get one layer of calls though...
+    // ntohl. this will only get one layer of calls though...
     val srcs = filteredMethods.name.l.map(cpg.call.name(_))
     val sink = cpg.call.name("memcpy").argument.order(3)
 
